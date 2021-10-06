1ad2223b549352383676b6783ecd9ba1a85ae76d
Eve <yydxhhuyi@gmail.com>
2021-10-06 15:28:38 -0400

    custom environment

--- env/model.py
+++ env/model.py
@@ -36,6 +36,16 @@ class StarNetwork:
         self.C1 = 0.2
         self.C2 = 30
 
+        self.D = self.L + self.C2
+        for i in range(n_devices):
+            self.D[i,i] = 0
+
+        self.R = self.R + self.C1
+        for i in range(self.n_devices):
+            self.R[i,i] = 0
+
+        self.S = np.random.uniform(1,3, self.n_devices)
+
 
     def central_delay(self, n_bytes, i, j):
         if i == j:
@@ -46,42 +56,36 @@ class StarNetwork:
         return self.L[i,j] + n_bytes * self.R[i,j] + self.central_delay(n_bytes, i, j)
     
     def get_delay_matrix(self):
-        B = self.L + self.C2
-        for i in range(N):
-            B[i,i] = 0
-        return B
+        return self.D
     
-    def get_rate_matrix(self, nodes=None):
-        R =  self.R + self.C1
-        for i in range(N):
-            R[i,i] = 0
-        if nodes:
-            return R[nodes][:,nodes]
-        return R
+    def get_rate_matrix(self):
+        return self.R
 
 
 class Program:
-    def __init__(self, n_operators, network):
-        self.n_operators = n_operators
+    def __init__(self, edge_list, network):
         self.P = nx.DiGraph()
-        self.P.add_edges_from([(0, 1), (0, 2), (0, 3), (1, 4), (1, 6), (2, 4), (2, 3), (3, 5), (4, 5), (5, 6)])
+        self.P.add_edges_from(edge_list)
+        self.n_operators = self.P.number_of_nodes()
 #         self.P.add_edges_from([(i, i+1) for i in range(n_operators-1)])
 
-        self.B = np.zeros((n_operators, n_operators))
+        self.B = np.zeros((self.n_operators, self.n_operators))
         for e in self.P.edges:
             self.P.edges[e]['bytes'] = np.random.uniform(500, 1000)
             self.B[e] = self.P.edges[e]['bytes']
 
         k = network.n_types
-        self.placement_constraints = [np.random.choice(k, k//2 + (np.random.sample()> 0.5) * 1 - (np.random.sample()> 0.5) * 1) for i in range(n_operators)]
-        for i in range(n_operators):
+        self.placement_constraints = [np.random.choice(k, k//2 + (np.random.sample()> 0.5) * 1 - (np.random.sample()> 0.5) * 1) for i in range(self.n_operators)]
+        for i in range(self.n_operators):
             self.placement_constraints[i] = set().union(*[network.groups[j] for j in self.placement_constraints[i]])
 
-        self.T = np.zeros([n_operators, network.n_devices])
-        for i in range(n_operators):
-            self.T[i, list(self.placement_constraints[i])] = np.random.exponential(50, len(self.placement_constraints[i]))
-            self.T[self.T==0] = np.inf
+        self.A = np.random.exponential(100, self.n_operators)
 
+        self.T = np.zeros([self.n_operators, network.n_devices])
+        for i in range(self.n_operators):
+            for j in self.placement_constraints[i]:
+                self.T[i,j] = self.A[i]/network.S[j]
+            self.T[self.T==0] = np.inf
         self.pinned = [np.random.choice(list(self.placement_constraints[0])), np.random.choice(list(self.placement_constraints[-1]))]
 
     def random_mapping(self):
--- main.py
+++ main.py
@@ -1,6 +1,107 @@
+import numpy as np
+
 import gym
 from gym.envs.registration import register
 
+from env.model import Program, StarNetwork
+from utils import *
 
-register(id='Placement-v0', entry_point='env.placement_env:PlacementEnv')
-env = gym.make('Placement-v0')
+class PlacementEnv:
+    def __init__(self, n_devices, n_types, edge_list, seed=0):
+
+        self.network = StarNetwork(n_devices, n_types)
+        self.program = Program(edge_list, self.network)
+
+        self.graph = self.program.P
+
+        self.n_operators = self.program.n_operators
+        self.n_devices = self.network.n_devices
+
+        self.seed = seed
+        self.mapping = None
+        self.cpath = None
+        self.op_idx = None
+
+        self.done = False
+        self.max_steps = 30 * self.n_operators
+        self.step_count = 0
+
+        self.op_neighbors = []
+        self.op_edges = []
+
+        for n in range(self.n_operators):
+            self.op_neighbors.append(list(self.graph.predecessors(n)) + list(self.graph.successors(n)))
+            self.op_edges.append(list(self.graph.in_edges(n)) + list(self.graph.out_edges(n)))
+
+
+    def reset(self, mapping = None):
+        self.done = False
+        self.step_count = 0
+        if mapping:
+            self.mapping = mapping
+        else:
+            self.mapping = self.program.random_mapping()
+
+        _, self.cpath = evaluate_maxP(from_mapping_to_matrix(self.mapping, self.n_devices), self.program, self.network)
+        self.op_idx = 1
+        central_op = self.cpath[1]
+        neighbors = self.op_neighbors[central_op]
+        edges = self.op_edges[central_op]
+        self.state = {
+            'op': [central_op, self.program.A[central_op]],
+            'constraint': self.program.placement_constraints[central_op],
+            'n_op': {n: self.program.A[n] for n in neighbors},
+            'n_e': {e: self.program.B[e] for e in edges},
+            'map': self.mapping
+        }
+        return self.state
+
+    def cpc_cost(self, ops, devs):
+        assert len(ops) == 3 and len(devs) == 3
+        s1, o, s2 = ops
+        d1, d, d2 = devs
+        return self.network.communication_delay(self.program.B[s1, o], d1, d) + self.network.communication_delay(self.program.B[o, s2], d, d2) + self.program.T[o, d]
+
+
+    def step(self, action):
+        op = self.state['op'][0]
+        assert action in self.program.placement_constraints[op]
+
+        pre = self.mapping[op]
+        self.mapping[op] = action
+
+        reward, cpath = evaluate_maxP(from_mapping_to_matrix(self.mapping, self.n_devices), self.program, self.network)
+        reward = -reward
+
+        if self.op_idx < len(self.cpath) - 2:
+            self.op_idx += 1
+        else:
+            self.cpath = cpath
+            self.op_idx = 1
+        central_op = self.cpath[self.op_idx]
+        neighbors = self.op_neighbors[central_op]
+        edges = self.op_edges[central_op]
+        next_state = {
+            'op': [central_op, self.program.A[central_op]],
+            'constraint': self.program.placement_constraints[central_op],
+            'n_op': {n: self.program.A[n] for n in neighbors},
+            'n_e': {e: self.program.B[e] for e in edges},
+            'map': self.mapping
+        }
+        self.state = next_state
+
+        done = self.step_count > self.max_steps
+        info = {'mapping_change': [op, pre, action]}
+        return next_state, reward, done, info
+
+
+
+N = 50
+k = 5
+edge_list = [(0, 1), (0, 2), (0, 3), (1, 4), (1, 6), (2, 4), (2, 3), (3, 5), (4, 5), (5, 6)]
+
+
+env = PlacementEnv(N, k, edge_list)
+
+# register(id='Placement-v0', entry_point='env.placement_env:PlacementEnv')
+# env = gym.make('Placement-v0')
