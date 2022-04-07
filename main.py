d51dc1def2cffdb0d324bc603caab5056723f19b
Eve <yydxhhuyi@gmail.com>
2022-04-06 22:17:03 -0400

    update data

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ from placement_rl.placement_agent import PlacementAgent
 import os
 import pickle
 
+from re import A
+from placement_rl.gnn import OpNet, reset
+from placement_rl.rl_agent import SoftmaxActor
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -30,6 +34,123 @@ for n in n_devices:
             DAG, constraints = generate_program(m, n, seed=seed)
             programs[n][m].append(Program(DAG, constraints))
 
+output_dim = 10
+
+program = programs[10][10][0]
+network = networks[10]
+env = PlacementEnv([network], [program])
+
+device_network = OpNet(env.get_node_feature_dim(), env.get_edge_feature_dim(), output_dim).to(device)
+
+dev_policy = SoftmaxActor(output_dim).to(device)
+
+dev_optim = torch.optim.Adam(list(device_network.parameters())+list(dev_policy.parameters()), lr=0.03)
+
+num_episodes = 20
+num_iterations = 50
+
+mapping = program.random_mapping()
+print(program.placement_constraints)
+
+last_latency, path, G_stats = env.simulate(0, 0, mapping, 0)
+env.get_full_graph(0, 0, mapping, path, G_stats)
+
+def train_one(env,
+          init_mapping,
+          episodes,
+          max_iter=50,
+          use_baseline=True,
+          noise=0,
+          gamma=0.95):
+    op_rewards = []
+
+    map_records = [init_mapping]
+    lat_records = []
+    act_records = []
+
+    program = env.programs[0]
+    network = env.networks[0]
+
+    mask_dev = torch.zeros(network.n_devices).to(device)
+
+    currrent_stop_iter = max_iter
+
+    node_dict = env.full_graph_node_dict[0][0]
+    action_dict = {}
+    for o in node_dict:
+        for d in node_dict[o]:
+            action_dict[node_dict[o][d]] = (o, d)
+
+    for i in range(episodes):
+        cur_mapping = init_mapping.copy()
+        last_latency, path, G_stats = env.simulate(0, 0, init_mapping, noise)
+
+        print(f'=== Episode {i} ===')
+        latencies = [last_latency]
+        actions = []
+        ep_reward = 0
+
+        mask_op = torch.ones(program.n_operators).to(device)
+        mask_op[0] = mask_op[-1] = 0
+
+        saved_rewards = []
+        dev_log_probs = []
+
+        for t in range(currrent_stop_iter):
+            temp_mapping = cur_mapping.copy()
+            g1 = env.get_full_graph(0, 0, temp_mapping, path, G_stats).to(device)
+            dev_embedding = device_network(g1).to(device)
+            probs = dev_policy(dev_embedding).to(device)
+            m = torch.distributions.Categorical(probs=probs)
+            # action = list(node_dict[s].keys())[m.sample().item()]
+
+            a = m.sample()
+            dev_log_probs.append(m.log_prob(a))
+            s, action = action_dict[a.item()]
+            cur_mapping[s] = action
+            latency, path, G_stats = env.simulate(0, 0, cur_mapping, noise)
+            reward = (last_latency - latency) / 10
+            last_latency = latency
+
+            latencies.append(latency)
+            actions.append([s, action])
+            saved_rewards.append(reward)
+            ep_reward = reward + ep_reward * gamma
+
+        R = 0
+        dev_policy_loss = 0
+        returns = []
+        for r in saved_rewards[::-1]:
+            R = r + gamma * R
+            returns.insert(0, R)
+
+        if use_baseline:
+            for i in range(len(saved_rewards)):
+                if i == 0:
+                    bk = saved_rewards[0]
+                else:
+                    try:
+                        bk = sum(saved_rewards[:i + 1]) / len(saved_rewards[:i + 1])
+                    except:
+                        bk = sum(saved_rewards) / len(saved_rewards)
+                returns[i] -= bk
+
+        returns = torch.tensor(returns).to(device)
+        # returns = (returns - returns.mean()) / (returns.std() + epsilon)
+
+        dev_optim.zero_grad()
+        for log_prob, R in zip(dev_log_probs, returns):
+            dev_policy_loss = dev_policy_loss - log_prob * R
+        dev_policy_loss.backward()
+        dev_optim.step()
+        op_rewards.append(ep_reward)
+        map_records.append(cur_mapping)
+        lat_records.append(latencies)
+        act_records.append(actions)
+
+    return op_rewards, lat_records, act_records, map_records
+
+op_rewards, lat_records, act_records, map_records = train_one(env, mapping, 100, max_iter=num_iterations, use_baseline=True, noise=0,  gamma=0.95)
 
 def train(env,
           agent,
